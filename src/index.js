441d9208b078779fcc17d5d10e48488c66390d55
Gavr1k <47858531+Gavr1k@users.noreply.github.com>
2021-03-04 20:55:29 +0300

    feat update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array == undefined || array.length == 0)
+    {
+        return 0;
+    }
+
+    let buf = array[0];
+    for(let i = 1; i < array.length; i++ )
+    {
+        if(buf > array[i])
+            buf = array[i];
+    }
+    return buf;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array == undefined || array.length == 0)
+    {
+        return 0;
+    }
+
+    let buf = array[0];
+    for(let i = 1; i < array.length; i++ )
+    {
+        if(buf < array[i])
+            buf = array[i];
+    }
+    return buf;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+  if(array == undefined || array.length == 0)
+    {
+        return 0;
+    }
+    let buf = array[0];
+    for(let i = 1; i < array.length; i++ )
+    {
+            buf += array[i];
+    }
+    return buf/array.length;
 }
